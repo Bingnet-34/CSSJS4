213f9a5e76119fa18bd5dadcdd30349170152426
khalil123-vip <138829112+khalil123-vip@users.noreply.github.com>
2024-02-28 21:29:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var password = prompt("ادخل كلمة المرور 👇:");
 if (password !== "Khalil") {
   window.location.replace("https://240aa944-03ff-4e38-902e-3e14b435bea9-00-if0y6xki3bho.janeway.replit.dev/");
 }
-var videos = document.getElementsByTagName('video, iframe');
+var videos = document.getElementsByTagName('video');
 for (var i = 0; i < videos.length; i++) {
   videos[i].addEventListener('play', function() {
     for (var j = 0; j < videos.length; j++) {
