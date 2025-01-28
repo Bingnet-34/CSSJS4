4a926ea0cdceace5241e62e11a0feee174d6a599
Bingnet-34 <138829112+Bingnet-34@users.noreply.github.com>
2025-01-28 01:05:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var password = prompt("ادخل كلمة المرور 👇:");
 
 if (password !== "Khalil") {
-  window.location.replace("https://240aa944-03ff-4e38-902e-3e14b435bea9-00-if0y6xki3bho.janeway.replit.dev/");
+  window.location.replace("www.khalilfg4.duckdns.org");
 }
 var videos = document.getElementsByTagName('video');
 for (var i = 0; i < videos.length; i++) {
